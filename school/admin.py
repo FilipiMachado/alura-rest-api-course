55f9f29772bf01d95b1cb7630e31e612909888e7
filipi <filipichip7@gmail.com>
2023-05-01 23:07:49 -0300

    structuring admin page

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from school.models import Student, Course
+
+class StudentsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'rg', 'cpf', 'birth_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 20
+
+class CoursesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'course_code', 'description')
+    list_display_links = ('id', 'course_code')
+    search_fields = ('course_code',)
+    
+admin.site.register(Student, StudentsAdmin)
+admin.site.register(Course, CoursesAdmin)
