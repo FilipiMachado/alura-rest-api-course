78ccea911697c66f7f3738afbe3e03589383dae5
filipi <filipichip7@gmail.com>
2023-05-02 10:52:42 -0300

    serialized student/course models

--- school/views.py
+++ school/views.py
@@ -1,6 +1,13 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from school.models import Student, Course
+from school.serializer import StudentSerializer, CourseSerializer
 
-def students(request):
-    if request.method == 'GET':
-        student = {'id': 1, 'name': 'John Doe'}
-        return JsonResponse(student)
+class StudentsViewSet(viewsets.ModelViewSet):
+    """ Show all students """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    
+class CoursesViewSet(viewsets.ModelViewSet):
+    """ Show all courses """
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from school.views import students
+from school.views import StudentsViewSet, CoursesViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('students', StudentsViewSet, basename='Students')
+router.register('courses', CoursesViewSet, basename='Courses')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('students/', students),
 ]
